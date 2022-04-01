dcb28039804caed8d7bffb1eeded8f37a6d1c060
Olga <nadolga.nl@gmail.com>
2022-04-01 16:10:40 +0200

    Add html end css

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import './app-info.css';
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Employee accounting in a company</h1>
+            <h2>Total employees: </h2>
+            <h2>Will receive a bonus: </h2>
+        </div>    
+    )
+}
+
+export default AppInfo;
---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,12 @@
+import './search-panel.css';
+
+const SearchPanel = () => {
+    return (
+        <input
+            type="text"
+            className="form-control"
+            placeholder="Search employee" />
+    )
+}
+
+export default SearchPanel;
